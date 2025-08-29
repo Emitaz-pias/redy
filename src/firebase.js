070d3efac86df94533896c8a33f9ea867dbcb30f
Emtiaz-pias <emtiazpias383@gmail.com>
2025-08-29 18:57:21 +0600

    done

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,8 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyCjKz796PCvTO9UFodZ49z1mNeBG482ROk",
@@ -9,11 +10,12 @@ const firebaseConfig = {
   projectId: "redy-b8f46",
   storageBucket: "redy-b8f46.firebasestorage.app",
   messagingSenderId: "913550199083",
-  appId: "1:913550199083:web:7ffb08f2809d85ee5e9d6e",
-  measurementId: "G-0RR84MQWLP"
+  appId: "1:913550199083:web:051770d700d844795e9d6e",
+  measurementId: "G-GJH9EM6SEF"
 };
 
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
